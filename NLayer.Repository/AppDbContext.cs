949b4ed6a56e4274333dc8da8598600fd15a7f0d
Burak Senturk <senturkburk@gmail.com>
2023-06-17 00:50:37 +0300

    Repository katmanı içeriği oluşturuldu

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        //startup dan db yolunu verebilmem için DbContextOptions içeren ctor oluşturduk.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            //var p = new Product() { ProductFeature = new ProductFeature() { } }   //product a bağlı ProductFeature üretmek istiyorsam bu şekil yapabilirim
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //konfigüreayonları uygula.
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NLayer.Repository/Seed/ProductFeatureSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seed
+{
+    internal class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(new ProductFeature 
+            {
+                Id = 1,
+                Color="Kırmızı",
+                Height=100,
+                Width=200,
+                ProductId=1,
+            },
+            new ProductFeature
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 300,
+                Width = 300,
+                ProductId = 2,
+            });
+        }
+    }
+}
