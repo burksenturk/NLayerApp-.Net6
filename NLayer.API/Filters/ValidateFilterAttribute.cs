8784c8658b771a8f7f02a84578a0251deaa51573
Burak Senturk <senturkburk@gmail.com>
2023-08-06 03:13:25 +0300

    VAlidasyon ve filter yapıldı. Program.cs de global işlemelr yapıldı. ayrıca dependency ler verildi

---
+++ NLayer.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NLayer.Core.DTOs;
+using System.ComponentModel;
+
+namespace NLayer.API.Filters
+{
+	public class ValidateFilterAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			if(!context.ModelState.IsValid) 
+			{
+				//artık elimde bir hata sınıfı var (errorsa gelince ModelError çıkıyor) SElectMany e kadar olan kısımda.
+				//Bu kod, hataları toplar.ModelState içindeki hataları tespit eder, bu hataları errors adlı bir liste içine toplar.
+				var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+
+				context.Result=new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(400, errors));
+
+			}
+		}
+	}
+}
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,4 +1,8 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NLayer.API.Filters;
+using NLayer.Core.DTOs;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -7,13 +11,20 @@ using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
 using NLayer.Service.Mapping;
 using NLayer.Service.Services;
+using NLayer.Service.Validations;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;  //EFcore kendi filterýný inaktif yaptým kendi Filterýmý kullanmak için yaptým bunu.
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +32,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();// <bununla karþýlasýrsan,bunu nesne örneði alacaksýn>
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>)); //generic olduklarý için typeof kullandýk
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddAutoMapper(typeof(MapProfile));
 
